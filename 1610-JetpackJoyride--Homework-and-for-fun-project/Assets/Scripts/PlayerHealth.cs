1d0294424700fd8766d513fd8fa80217e69d402b
Christopher <10642047@my.uvu.edu>
2020-10-10 16:47:32 -0600

    [Homework] Script 3 additional Classes (1/3) [PlayerHealth]
    Based on what you've learned so far in the class, create three new Scripts with the following class names:
    	•PlayerHealth
    	•PlayerScore
    	•CharacterMover
    I created the PlayerHealth C# script.
    I chose to create the playerHealth variable as public so it could be seen in the unity editor.
    I  also initialized it at 100.
    I added the script as a component to the main player so now the playerHealth variable can be seen when testing the game.

---
+++ 1610-JetpackJoyride--Homework-and-for-fun-project/Assets/Scripts/PlayerHealth.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerHealth : MonoBehaviour
+{
+    //This integer is public so it can be seen in the unity editor. I also initialized it at 100.
+    public int playerHealth = 100;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
