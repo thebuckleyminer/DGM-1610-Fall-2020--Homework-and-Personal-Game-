4953f19697bed8679b99648260b78c813ac48bbe
Christopher <10642047@my.uvu.edu>
2020-10-11 12:27:02 -0600

    [Homework] Script 3 additional Classes (2/3) [PlayerScore]
    Based on what you've learned so far in the class, create three new Scripts with the following class names:
    	•PlayerHealth
    	•PlayerScore
    	•CharacterMover
    - I created the PlayerScore C# script.
    I want two scores for this game. 1) Coins collected for the run, 2) Distance Ran.
    I created the following variables:
    	- int coinCounter
    	- float distanceTraveled
    	- float gameStartingTime
    	- float distanceScaler
    Using these I made it so the distanceTraveled goes up over time. I know it is not accurate on the actual meters traveled, but I think it goes up at a good scale for the pace.
    I also started a coinCounter to keep track of and hopefully increment the coins when I hit them.

---
+++ 1610-JetpackJoyride--Homework-and-for-fun-project/Assets/Scripts/PlayerScore.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+public class PlayerScore : MonoBehaviour
+{
+    public int coinCounter = 0;
+    public float distanceTraveled = 0;
+    private float gameStartingTime;
+    private float distanceScaler = 10.0f;
+    
+
+    void Start()
+    {
+        gameStartingTime = Time.time;
+    }
+    void Update()
+    {
+        distanceTraveled = Mathf.RoundToInt((Time.time - gameStartingTime) * distanceScaler);
+        
+        //print(distanceTraveled); //THIS PRINT IS FOR DEBUGGING PURPOSES ONLY
+    }
+
+    public void CoinCollision()
+    {
+        coinCounter++;
+    }
+}
