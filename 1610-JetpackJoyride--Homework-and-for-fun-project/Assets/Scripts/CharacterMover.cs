bda4dc5fa9916286afa89eebd5793978f98c00ec
Christopher <10642047@my.uvu.edu>
2020-10-12 14:11:44 -0600

    [Homework] Script 3 additional Classes (3/3) [CharacterMover]
    Based on what you've learned so far in the class, create three new Scripts with the following class names:
    	•PlayerHealth
    	•PlayerScore
    	•CharacterMover
    
    - Since I already had the CharacterMover C# script in my game from class and creating this project, I spent some time and cleaned it up.
    - I added comments to everything, and I made the if statements one line instead of many.

--- 1610-JetpackJoyride--Homework-and-for-fun-project/Assets/Scripts/CharacterMover.cs
+++ 1610-JetpackJoyride--Homework-and-for-fun-project/Assets/Scripts/CharacterMover.cs
@@ -6,27 +6,22 @@ public class CharacterMover : MonoBehaviour
     //Public Variables
     public CharacterController controller;
     public float moveSpeed = 5.0f, gravity = -9.81f, jumpForce = 5.0f;
+    
     //Private Variables
     private Vector3 moveDirection;
     private float yDirection;
+    
     private void Update()
     {
-        var moveSpeedInput = moveSpeed * Input.GetAxis("Horizontal"); // This is going to make it so our character can go left and right when they push the buttons
-        moveDirection.Set(moveSpeedInput,yDirection,0);
-        yDirection += gravity * Time.deltaTime;
-        if (controller.isGrounded && moveDirection.y < 0)
-        {
-            yDirection = -1f;
-        }
-        if (Input.GetButtonDown("Jump"))
-        {
-            yDirection = jumpForce;
-        }
-        var movement = moveDirection * Time.deltaTime;
-        controller.Move(movement);
-
-        //Old Code:
-        //var movement = moveDirection * Time.deltaTime; // We put this here because you should not pass calculations
-        //controller.Move(movement);
+        // LEFT and RIGHT MOVEMENT
+        var moveSpeedInput = moveSpeed * Input.GetAxis("Horizontal"); // Takes player input and uses it as a scaler to make the character move
+        moveDirection.Set(moveSpeedInput,yDirection,0); // Converts the player input into a left or right movement
+        
+        // GRAVITY and JUMPING
+        yDirection += gravity * Time.deltaTime; // Adds gravity every second to simulate the acceleration of adding 9.81m/s^2 to the y in the downward force
+        if (controller.isGrounded && moveDirection.y < 0) { yDirection = -1f; } // Small downward force if player is grounded
+        if (Input.GetButtonDown("Jump")) { yDirection = jumpForce; } // Makes the character jump when the spacebar is pressed
+        var movement = moveDirection * Time.deltaTime; // Uses Time.deltaTime as a scaler to make it more uniform
+        controller.Move(movement); // Finaly this sends the movement to the character
     }
 }
