eaa91208c90d05d65afcc21d07d019212534fd7a
Christopher <10642047@my.uvu.edu>
2020-10-13 13:16:59 -0600

    [Homework] Write game variables	(1/4) [Health as a float]
    In a single script write variables for the following:
    	•Health as a float
    	•Password as a string
    	•Player name as a string
    	•Points as an int
    - For my daily practice I wrote a Health float variable.

---
+++ 1610-JetpackJoyride--Homework-and-for-fun-project/Assets/Scripts/PlayerInfo.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerInfo : MonoBehaviour
+{
+    //Creating Variables Practice
+    public float health = 500.0f;
+    
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
