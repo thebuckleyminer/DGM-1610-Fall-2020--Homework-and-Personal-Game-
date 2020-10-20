2a64cfc38ac4cc04d3216a1bafbea71580214e4b
Christopher <10642047@my.uvu.edu>
2020-10-19 20:08:01 -0600

    [Homework] Write a health power-up
    Write a script that uses the method OnTriggerEnter.
    Right lines of code that will increase the health variable.
    
    - Created the HealthPowerUp C# script.-
    - Made a healthValue integer variable and initialized at the value of 50.
    - Made the OnTriggerEnter function.
    - Inside the function I have it so whenever the trigger happens the healthValue variable increases by 20.
    - I also added the line: Destroy(other.gameObject); to remove the powerup it touched.
    - I placed the C# script onto the player as a component and then I added a Health Power-Up sphere primitive with a trigger and tested to make sure the value incremented and the powerup got destroyed.

---
+++ 1610-JetpackJoyride--Homework-and-for-fun-project/Assets/Scripts/HealthPowerUp.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HealthPowerUp : MonoBehaviour
+{
+    public int healthValue = 50;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        healthValue += 20;
+        Destroy(other.gameObject);
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
+
+//
+//[Homework] Write a health power-up
+//Write a script that uses the method OnTriggerEnter.
+//Right lines of code that will increase the health variable.
