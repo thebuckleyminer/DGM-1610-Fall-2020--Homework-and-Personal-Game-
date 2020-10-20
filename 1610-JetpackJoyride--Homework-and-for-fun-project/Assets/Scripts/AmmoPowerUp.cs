846ee22a59a05b59a07679baf00180b9ba9ae70f
Christopher <10642047@my.uvu.edu>
2020-10-20 08:13:41 -0600

    [Homework] Write a second Power-Up
    - Created the AmmoPowerUp C# script.
    - Initialized the ammo value variable: (public int ammoValue = 10;)
    - Made a list of different values that can randomally be choosen to add to the ammo: (public int[] ammoBoostRandomChoice = {5, 10, 15, 20, 25};)
    - I made my own function: AmmoValueIncrease() that chooses a random number 0 to 4 as a index to the list above and then adds the value from the location in the list to the ammo. (var ammoIndex = Random.Range(0, 5);	ammoValue += ammoBoostRandomChoice[ammoIndex];)
    - Lastly I wrote the OnTriggerEnter to call my AmmoValueIncrease() function once and then to Destroy(other.gameObject) so the power-up disappears.
    - I then place the on the player object as a component.
    - I added a cylinder named Ammo Power-Up and checked is trigger and tested to see if the powerup worked.

---
+++ 1610-JetpackJoyride--Homework-and-for-fun-project/Assets/Scripts/AmmoPowerUp.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Random = UnityEngine.Random;
+
+
+public class AmmoPowerUp : MonoBehaviour
+{
+    public int ammoValue = 10;
+    public int[] ammoBoostRandomChoice = {5, 10, 15, 20, 25};
+    private void AmmoValueIncrease()
+    {
+        var ammoIndex = Random.Range(0, 5);
+        ammoValue += ammoBoostRandomChoice[ammoIndex];
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        AmmoValueIncrease();
+        Destroy(other.gameObject);
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
