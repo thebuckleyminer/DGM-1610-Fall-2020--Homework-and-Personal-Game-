4740f46ad3cb87d1f2568729489b395f6df8e34a
Christopher <10642047@my.uvu.edu>
2020-10-21 12:35:07 -0600

    [Homework] Write a Third Power-up
    Based on what you've learned so far in the class, write a power-up like the other power up to you previously written.
    - Created the ThirdPowerUp C# script
    - I thought a useful power-up object to hit might be to make you bigger... so this scales your size by 2!
    - I created a private DoubleInSize() function to set the scale to x2 on the x and y axis through a Vector3 and then...
    - When I made the OnTriggerEnter function I had it call the DoubleInSize() function to run that code, then finally the Destroy(other.gameObject) to make it so that power-up could not be used more than once.
    - I applied the script as a component to my player as well as made a new cube named Enlarge Power-Up with a trigger.
    - I then tested hitting this cube and made me twice as big!

---
+++ 1610-JetpackJoyride--Homework-and-for-fun-project/Assets/Scripts/ThirdPowerUp.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ThirdPowerUp : MonoBehaviour
+{
+    // The purpose of this power-up is to Double in size
+    private void DoubleInSize()
+    {
+        transform.localScale = new Vector3(transform.localScale.x*2,transform.localScale.y*2,transform.localScale.z);
+        print("Zoom");
+    }
+    private void OnTriggerEnter(Collider other)
+    {
+        Destroy(other.gameObject);
+        DoubleInSize();
+        
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
