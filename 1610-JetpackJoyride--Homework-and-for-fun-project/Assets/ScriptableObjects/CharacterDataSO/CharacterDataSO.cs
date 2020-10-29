ea30fed74d5861dab07bfc9552d28368bd0adf80
Christopher <10642047@my.uvu.edu>
2020-10-29 10:19:11 -0600

    [Homework] Script a character Data SO
    - Created the CharacterDataSO C# script.
    - Made the CharacterCurrentData object.
    - This object contains a bool for the playerOffGround, a Sprite for the currentSkin, a Color for the powerUpColor, a int for the currentHeight, and a  float for the playerHealth.

---
+++ 1610-JetpackJoyride--Homework-and-for-fun-project/Assets/ScriptableObjects/CharacterDataSO/CharacterDataSO.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+[CreateAssetMenu]
+public class CharacterDataSO : ScriptableObject
+{
+    public bool playerOffGround = false;
+    public Sprite currentSkin;
+    public Color powerUpColor = Color.white;
+    public int currentHeight = 2;
+    public float playerHealth = 1.0f;
+}
