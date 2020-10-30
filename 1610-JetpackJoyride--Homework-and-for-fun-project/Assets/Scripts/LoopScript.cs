b3d469cdec7955830f06397838c7ec38d9e6225f
Christopher <10642047@my.uvu.edu>
2020-10-30 10:31:05 -0600

    [Homework] Script data to loop
    Created a LoopScript to continually update a game integer unless the game is over.

---
+++ 1610-JetpackJoyride--Homework-and-for-fun-project/Assets/Scripts/LoopScript.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LoopScript : MonoBehaviour
+{
+    public bool gameOverBool = false;
+
+    public int totalUpdateRunScore = 0;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        while (!gameOverBool)
+        {
+            totalUpdateRunScore++;
+        }
+    }
+
+    //public IEnumerator gameRunningLoop(bool gameOverBool)
+    //{
+    //    while (!gameOverBool)
+    //    {
+    //        yield return new WaitForSeconds(10);
+    //        print("Ten seconds have passed");
+    //    }
+    //}
+}
