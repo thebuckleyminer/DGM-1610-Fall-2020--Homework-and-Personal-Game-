b5406ca778bdf0a154e0c735c67056caca6ff9b1
Christopher <10642047@my.uvu.edu>
2020-11-01 16:06:37 -0700

    [Homework] Script a collection loop
    I scripted some weapons as scriptiable objects and then wrote a C# Script to have it so the weapons cycled through and give each weapon the int value of 100. In the future I would be able to use this as a reload power-up.

---
+++ 1610-JetpackJoyride--Homework-and-for-fun-project/Assets/ScriptableObjects/Weapons/CollectibleWeaponSOScript.cs
@@ -0,0 +1,9 @@
+﻿using UnityEngine;
+[CreateAssetMenu]
+public class CollectibleWeaponSOScript : ScriptableObject
+{
+    public int ammo;
+    public Sprite weaponImage;
+    public Color currentColor = Color.white;
+    public bool activeWeapon = false;
+}
---
+++ 1610-JetpackJoyride--Homework-and-for-fun-project/Assets/Scripts/AddMaxAmmoToAll.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using UnityEngine;
+
+public class AddMaxAmmoToAll : MonoBehaviour
+{
+    public int howdy;
+
+    public List<CollectibleWeaponSOScript> collectibleWeapons;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void reloadAll()
+    {
+        for (var i = 0; i < collectibleWeapons.Count; i++)
+        {
+            var indexWeapon = collectibleWeapons[i];
+            indexWeapon.ammo = 100;
+        }
+    }
+}
