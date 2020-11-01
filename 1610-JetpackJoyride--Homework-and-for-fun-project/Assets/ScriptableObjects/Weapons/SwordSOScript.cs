32dfd6705d739d6e2f636ca5b2f609b8b7962312
Christopher <10642047@my.uvu.edu>
2020-11-01 16:10:31 -0700

    [Homework] Script collectable data SO
    Since I had already created the weapon SO script for the gun weapons, I am going to use a new SwordSOScript to make swords.
    I used a float for the swordLength, a  Color for the swordColor defaulted to Color.white, and a Sprite for the swordDesign;

---
+++ 1610-JetpackJoyride--Homework-and-for-fun-project/Assets/ScriptableObjects/Weapons/SwordSOScript.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+[CreateAssetMenu]
+public class SwordSOScript : ScriptableObject
+{
+    public float swordLength;
+    public Color swordColor = Color.white;
+    public Sprite swordDesign;
+}
