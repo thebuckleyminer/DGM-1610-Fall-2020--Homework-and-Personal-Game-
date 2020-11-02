88c1c5cc597d3d93d9990af64147d85ccbddf878
Christopher <10642047@my.uvu.edu>
2020-11-02 08:38:02 -0700

    [Homework] Script a UI Interaction
    I created a score text value that updates with the time elapsed.
    I also created a cash and a dialogue areas to be used in the future.

---
+++ 1610-JetpackJoyride--Homework-and-for-fun-project/Assets/Scripts/ScoreUpdater.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ScoreUpdater : MonoBehaviour
+{
+    public float startTime;
+    public float elapsedTime;
+
+    public Text scoreText;
+    // Start is called before the first frame update
+    void Start()
+    {
+    startTime = Time.time;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        elapsedTime = Time.time - startTime;
+        scoreText.text = elapsedTime.ToString();
+    }
+}
