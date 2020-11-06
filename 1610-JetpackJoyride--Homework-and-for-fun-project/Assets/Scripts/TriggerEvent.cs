643c56a0461ee89e674ddc379c923156acce6aa6
Christopher <10642047@my.uvu.edu>
2020-11-06 08:51:12 -0700

    [Homework] Write an Event
    - Created the TriggerEvent Script

---
+++ 1610-JetpackJoyride--Homework-and-for-fun-project/Assets/Scripts/TriggerEvent.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class TriggerEvent : MonoBehaviour
+{
+    public UnityEvent triggerEnterEvent, triggerStayEvent, triggerExitEvent, awakeEvent;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        triggerEnterEvent.Invoke();
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        triggerStayEvent.Invoke();
+    }
+
+    private void OnTriggerStay(Collider other)
+    {
+        triggerExitEvent.Invoke();
+    }
+
+    private void Awake()
+    {
+        awakeEvent.Invoke();
+    }
+}
