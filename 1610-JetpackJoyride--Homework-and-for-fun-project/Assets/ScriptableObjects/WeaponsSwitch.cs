a44e12ae910fa2be63836a312ffe6211b969ebec
Christopher <10642047@my.uvu.edu>
2020-11-11 11:40:49 -0700

    [Homework] Script a weapons switch
    Implemented a weapons switch

---
+++ 1610-JetpackJoyride--Homework-and-for-fun-project/Assets/ScriptableObjects/WeaponsSwitch.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using UnityEngine;
+[CreateAssetMenu]
+public class WeaponsSwitch : ScriptableObject
+{
+    public enum Weapons
+    {
+        Guns,
+        Mistles,
+        Spikes
+    }
+
+    public Weapons currentDefence;
+
+    public void CaseCurrentWeapons()
+    {
+        switch (currentDefence)
+        {
+            case Weapons.Guns:
+                // Function
+                break;
+            case Weapons.Mistles:
+                // Function
+                break;
+            case Weapons.Spikes:
+                // Function
+                break;
+
+        }
+    }
+}
