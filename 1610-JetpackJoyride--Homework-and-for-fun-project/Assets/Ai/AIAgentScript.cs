0b0b6d43c167bf5d3d50da04f0964546d2433b60
Christopher <10642047@my.uvu.edu>
2020-11-13 11:53:29 -0700

    [Homework] Script AI
    Worked on the AI Script and functionality

---
+++ 1610-JetpackJoyride--Homework-and-for-fun-project/Assets/Ai/AIAgentScript.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+[RequireComponent(typeof(NavMeshAgent))]
+public class AIAgentScript : MonoBehaviour
+{
+    private NavMeshAgent agent;
+    public Transform destination;
+    
+    
+    private void Start()
+    {
+        agent = GetComponent<NavMeshAgent>();
+        destination = transform;
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        destination = other.transform;
+    }
+    
+    
+    private void Update()
+    {
+        agent.destination = destination.position;
+    }
+}
