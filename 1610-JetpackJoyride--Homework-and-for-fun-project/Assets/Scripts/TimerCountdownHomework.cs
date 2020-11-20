b00f2b9adcf1d00fe1327b7b8019f9b528b11d49
Christopher <10642047@my.uvu.edu>
2020-11-20 12:10:45 -0700

    [Homework] Use the Counter Script to Create a GUI Countdown
    I made a ten second countdown that has a message at the beginning and the end.

---
+++ 1610-JetpackJoyride--Homework-and-for-fun-project/Assets/Scripts/TimerCountdownHomework.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TimerCountdownHomework : MonoBehaviour
+{
+    private Text timerText;
+    public int secondsBetweenShowing = 1;
+    IEnumerator Start()
+    {
+        timerText = GetComponent<Text>();
+        timerText.text = "Get Ready!";
+        yield return new WaitForSeconds(secondsBetweenShowing);
+        yield return new WaitForSeconds(secondsBetweenShowing);
+        timerText.text = "10";
+        var counterNumber = 10;
+        while (counterNumber>0)
+        {
+            timerText.text = counterNumber.ToString();
+            yield return new WaitForSeconds(secondsBetweenShowing);
+            counterNumber--;
+        }
+        timerText.text = "Blast Off!";
+
+    }
+}
